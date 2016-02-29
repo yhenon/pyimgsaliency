3d9ed680a002be577542071548bf4f2b5e218547
Yann <yannhenon@gmail.com>
2016-02-29 10:38:30 -0500

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pyImSaliency',
+setup(name='pyimgsaliency',
       version='0.1',
       description='A package for calculating image saliency',
-      url='https://github.com/yhenon/pyImSaliency',
+      url='https://github.com/yhenon/pyimgsaliency',
       author='Yann Henon',
       author_email='none',
       license='Apache',
-      packages=['pyImSaliency'],
+      packages=['pyimgsaliency'],
       zip_safe=False)
